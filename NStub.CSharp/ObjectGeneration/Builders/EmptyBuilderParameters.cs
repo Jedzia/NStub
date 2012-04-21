b375edc5a1253ba9c1646cb4638fa890064ccb89
Jedzia <jed69@gmx.de>
2012-04-21 14:48:31 +0200

    EmptyBuilderParametersBase<T> base for IMemberBuilder property parameters

--- NStub.CSharp/ObjectGeneration/Builders/EmptyBuilderParameters.cs
+++ NStub.CSharp/ObjectGeneration/Builders/EmptyBuilderParameters.cs
@@ -9,12 +9,214 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using System.Xml.Serialization;
+using System.IO;
 namespace NStub.CSharp.ObjectGeneration.Builders
 {
+    #region Base entity class
+    public partial class BuilderParametersBase<T>
+    {
+
+        private static System.Xml.Serialization.XmlSerializer serializer;
+
+        private static System.Xml.Serialization.XmlSerializer Serializer
+        {
+            get
+            {
+                if ((serializer == null))
+                {
+                    serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+                }
+                return serializer;
+            }
+        }
+
+        #region Serialize/Deserialize
+        /// <summary>
+        /// Serializes current EntityBase object into an XML document
+        /// </summary>
+        /// <returns>string XML value</returns>
+        public virtual string Serialize()
+        {
+            System.IO.StreamReader streamReader = null;
+            System.IO.MemoryStream memoryStream = null;
+            try
+            {
+                memoryStream = new System.IO.MemoryStream();
+                Serializer.Serialize(memoryStream, this);
+                memoryStream.Seek(0, System.IO.SeekOrigin.Begin);
+                streamReader = new System.IO.StreamReader(memoryStream);
+                return streamReader.ReadToEnd();
+            }
+            finally
+            {
+                if ((streamReader != null))
+                {
+                    streamReader.Dispose();
+                }
+                if ((memoryStream != null))
+                {
+                    memoryStream.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Deserializes workflow markup into an EntityBase object
+        /// </summary>
+        /// <param name="xml">string workflow markup to deserialize</param>
+        /// <param name="obj">Output EntityBase object</param>
+        /// <param name="exception">output Exception value if deserialize failed</param>
+        /// <returns>true if this XmlSerializer can deserialize the object; otherwise, false</returns>
+        public static bool Deserialize(string xml, out T obj, out System.Exception exception)
+        {
+            exception = null;
+            obj = default(T);
+            try
+            {
+                obj = Deserialize(xml);
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                exception = ex;
+                return false;
+            }
+        }
+
+        public static bool Deserialize(string xml, out T obj)
+        {
+            System.Exception exception = null;
+            return Deserialize(xml, out obj, out exception);
+        }
+
+        public static T Deserialize(string xml)
+        {
+            System.IO.StringReader stringReader = null;
+            try
+            {
+                stringReader = new System.IO.StringReader(xml);
+                return ((T)(Serializer.Deserialize(System.Xml.XmlReader.Create(stringReader))));
+            }
+            finally
+            {
+                if ((stringReader != null))
+                {
+                    stringReader.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Serializes current EntityBase object into file
+        /// </summary>
+        /// <param name="fileName">full path of outupt xml file</param>
+        /// <param name="exception">output Exception value if failed</param>
+        /// <returns>true if can serialize and save into file; otherwise, false</returns>
+        public virtual bool SaveToFile(string fileName, out System.Exception exception)
+        {
+            exception = null;
+            try
+            {
+                SaveToFile(fileName);
+                return true;
+            }
+            catch (System.Exception e)
+            {
+                exception = e;
+                return false;
+            }
+        }
+
+        public virtual void SaveToFile(string fileName)
+        {
+            System.IO.StreamWriter streamWriter = null;
+            try
+            {
+                string xmlString = Serialize();
+                System.IO.FileInfo xmlFile = new System.IO.FileInfo(fileName);
+                streamWriter = xmlFile.CreateText();
+                streamWriter.WriteLine(xmlString);
+                streamWriter.Close();
+            }
+            finally
+            {
+                if ((streamWriter != null))
+                {
+                    streamWriter.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Deserializes xml markup from file into an EntityBase object
+        /// </summary>
+        /// <param name="fileName">string xml file to load and deserialize</param>
+        /// <param name="obj">Output EntityBase object</param>
+        /// <param name="exception">output Exception value if deserialize failed</param>
+        /// <returns>true if this XmlSerializer can deserialize the object; otherwise, false</returns>
+        public static bool LoadFromFile(string fileName, out T obj, out System.Exception exception)
+        {
+            exception = null;
+            obj = default(T);
+            try
+            {
+                obj = LoadFromFile(fileName);
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                exception = ex;
+                return false;
+            }
+        }
+
+        public static bool LoadFromFile(string fileName, out T obj)
+        {
+            System.Exception exception = null;
+            return LoadFromFile(fileName, out obj, out exception);
+        }
+
+        public static T LoadFromFile(string fileName)
+        {
+            System.IO.FileStream file = null;
+            System.IO.StreamReader sr = null;
+            try
+            {
+                file = new System.IO.FileStream(fileName, FileMode.Open, FileAccess.Read);
+                sr = new System.IO.StreamReader(file);
+                string xmlString = sr.ReadToEnd();
+                sr.Close();
+                file.Close();
+                return Deserialize(xmlString);
+            }
+            finally
+            {
+                if ((file != null))
+                {
+                    file.Dispose();
+                }
+                if ((sr != null))
+                {
+                    sr.Dispose();
+                }
+            }
+        }
+        #endregion
+    }
+    #endregion
+
     /// <summary>
     /// Provides an implementation of the <see cref="IMemberBuilderParameters"/> user data, that has an empty set of data.
     /// </summary>
-    public class EmptyBuilderParameters : EntityBase<EmptyBuilderParameters>, IMemberBuilderParameters
+    public class EmptyBuilderParameters : EmptyBuilderParametersBase<EmptyBuilderParameters>, IMemberBuilderParameters
+    {
+    }
+
+    /// <summary>
+    /// Base class for implementations of the <see cref="IMemberBuilderParameters"/> user data with a Enabled property, serialization
+    /// and deserialization capability.
+    /// </summary>
+    public class EmptyBuilderParametersBase<T> : BuilderParametersBase<T>, IMemberBuilderParameters where T : IMemberBuilderParameters
     {
         // : EntityBase<PropertyBuilderParametersSetup> {
         #region Properties
@@ -47,7 +249,7 @@ namespace NStub.CSharp.ObjectGeneration.Builders
         /// <param name="xml">The XML text, representing the data.</param>
         public new void Deserialize(string xml)
         {
-            EmptyBuilderParameters deserObj;
+            T deserObj;
             Deserialize(xml, out deserObj);
             this.Enabled = deserObj.Enabled;
         }
